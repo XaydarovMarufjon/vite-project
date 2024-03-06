dd6ca7be404b17e1b95e17d49b29b189345c29dc
XaydarovMarufjon <marufjankhaydarov@gmail.com>
2024-03-06 18:08:15 +0500

    feat: :heavy_plus_sign: create project learning

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [events, setEvents] = useState([
+    { title: "Dilo", id: 1 },
+    { title: "Diloshda", id: 2 },
+    { title: "Lobar", id: 3 },
+  ])
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+
+      {events.map(() => {
+        return (
+          1
+        )
+      })}
+
+
+    </>
+  )
+}
+
+export default App
